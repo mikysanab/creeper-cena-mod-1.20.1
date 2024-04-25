31024770d6abd34769d7afc56ad03804d1c5c825
Michael Sanabria <mikysanab@gmail.com>
2024-04-24 21:48:36 -0700

    initialize

---
+++ src/main/java/net/miky/creepercenamod/CreeperCenaMod.java
@@ -0,0 +1,14 @@
+package net.miky.creepercenamod;
+import net.fabricmc.api.ModInitializer;
+
+public class CreeperCenaMod implements ModInitializer {
+
+	// public static final String MOD_ID = "creepercenamod";
+	// public static final Logger LOGGER = LoggerFactory.getLogger("creeper-cena-mod");
+
+	@Override
+	public void onInitialize() {
+		//test
+		//test
+	}
+}
